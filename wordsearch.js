d6d4ecf7b37e3b0fe34fd4d0a6921541a6db1685
robotsch <christianhumble22@gmail.com>
2022-03-16 20:04:17 -0400

    add vertical wordsearch functionality, add tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  // Pair Programmed
+  // @law86, robotsch
 
-module.exports = wordSearch
+  // Convert each array to a string
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  // Loop through each string
+  for (const l of horizontalJoin) {
+    // Return true if word is included in string
+    if (l.includes(word)) return true;
+  }
+  // Loop through entire array
+  for (let i = 0; i < letters[0].length; i++) {
+    const newColumn = [];
+    // Loop through columns
+    for (let j = 0; j < letters.length; j++) {
+      // Add each letter to new array
+      newColumn.push(letters[j][i]);
+    }
+    // Return true if stringified array includes given word
+    if (newColumn.join("").includes(word)) return true;
+  }
+  // Return false after all checks
+  return false;
+};
+
+module.exports = wordSearch;
